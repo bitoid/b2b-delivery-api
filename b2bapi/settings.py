98d47a4ffc15ac3c8a9c8b5cd9bdbb1c7fd20535
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-06 23:22:02 +0400

    connect postgresql server

--- b2bapi/settings.py
+++ b2bapi/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'b2bapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'b2b_delivery_api',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres5261285',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
