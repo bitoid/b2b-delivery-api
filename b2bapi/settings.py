fcd016f87d17de94ca50c6ef114a8ee306a39740
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-11 01:11:04 +0400

    fix token authentication permissions

--- b2bapi/settings.py
+++ b2bapi/settings.py
@@ -127,7 +127,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
