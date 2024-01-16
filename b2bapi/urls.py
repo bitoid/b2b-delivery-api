b54d4fb6c66cf5cc1833f0930e6adf5f6a03b9c6
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-01-16 17:38:50 +0400

    add yasg ulr patterns

--- b2bapi/urls.py
+++ b2bapi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from delivery.views import ClientViewSet, CourierViewSet, OrderViewSet, UserViewSet, LoginView, ExcelUploadView
 from django.contrib import admin
+from .yasg import urlpatterns as doc_urls
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -15,3 +16,5 @@ urlpatterns = [
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/upload-excel/', ExcelUploadView.as_view(), name='upload-excel'),
 ]
+
+urlpatterns += doc_urls
