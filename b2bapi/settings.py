bc5a89072fe43216a76a525070f691dc081f1871
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-01-24 22:45:16 +0400

    remove pagination

--- b2bapi/settings.py
+++ b2bapi/settings.py
@@ -146,9 +146,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10
 }
 
 
