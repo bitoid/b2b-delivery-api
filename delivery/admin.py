a94313c3eab49eca079af41a96377d78f1ef442d
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-02-10 12:43:01 +0400

    add notification model to administration page

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client, Courier, Order
+from .models import Client, Courier, Order, Notification
 
 admin.site.register(Client)
 admin.site.register(Courier)
 admin.site.register(Order)
+admin.site.register(Notification)
