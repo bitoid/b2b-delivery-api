cdd433d14ee951eb9378456d90ce97bb5b45a88f
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-05-09 19:53:12 +0400

    change secret key

--- b2bapi/settings.py
+++ b2bapi/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1!=)h1($ua5+kckdefo%i%qgdj^8dmsyi)@2=e$1(fmrbo42f!"
+SECRET_KEY = 'django-insecure-1!=)h1($ua5+kckdefo%i%qgdj^8dmsyi)@2=e$1(fmrbo42f!'
 
 #SECRET_KEY = os.environ.get("SECRET_KEY")
 
