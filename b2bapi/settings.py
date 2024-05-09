e414a29cbf3e28f93cbceec560fd2a6afaadb20b
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-05-09 19:58:14 +0400

    secret key last update

--- b2bapi/settings.py
+++ b2bapi/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-from django.core.management.utils import get_random_secret_key
 
 # from dotenv import load_dotenv
 
@@ -14,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = 'djangosupersecretkegdofkkfddfgfdg'
 
 #SECRET_KEY = os.environ.get("SECRET_KEY")
 
